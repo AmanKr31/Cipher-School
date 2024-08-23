4dd41593a30ed40b5caebe601e4cb6dcbb1fac27
Aman Kumar <121360041+AmanKr31@users.noreply.github.com>
2024-08-23 11:11:58 +0530

    Add files via upload

---
+++ Lecture-3-PrimitiveDT/primitiveDT.java
@@ -0,0 +1,19 @@
+
+
+public class primitiveDT {
+    public static void main(String[] args){
+        int x=5;
+        int y=7;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+        byte b =10;
+        System.out.println("B is : "+b);
+
+        byte b1 =(byte)129;
+        System.out.println("B is : "+b1);
+
+        int i = b;
+        System.out.println("I is : "+i);
+    }
+}
