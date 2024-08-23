d46448488a21bb3292660eabbe342dfcf72b9762
Aman Kumar <121360041+AmanKr31@users.noreply.github.com>
2024-08-23 11:13:03 +0530

    Add files via upload

---
+++ Lecture-5-Operators/primitiveDt.java
@@ -0,0 +1,19 @@
+
+public class primitiveDt {
+    public static void main(String[] args) {
+        float f=(float)5.5;
+        float f2 = 5.5f;
+        System.out.println(f);
+        double d = 10.5;
+        char ch = 'a';
+        System.out.println(ch);
+
+        int m =5;
+        int n = m++;
+        System.out.println("M is: "+m+", N is: "+n);
+
+        int p= 5;
+        int q= ++p;
+        System.out.println("P is: "+p+", Q is: "+q);
+    }
+}
